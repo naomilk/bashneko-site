d9038a47d74e3e74987c50b19aac06dcdf9e235c
Naomi Kangwa <naomi@yawt.digital>
2021-11-23 14:07:01 +0000

    designing welcome page and name section begun

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [
     './**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      champagne: '#FBD1A2',
+      meringue: '#ECE4B7',
+      sandy: '#FC9F5B'
+    },
+    fontFamily: {
+      'DM-Sans': ['DM-Sans'],
+      'Dosis': ['Dosis'],
+      'Poppins': ['Poppins'],
+      'Rubik': ['Rubik']
+    },
+    extend: {
+      animation: ['hover'],
+    },
   },
   variants: {
     extend: {},
